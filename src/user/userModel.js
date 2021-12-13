6d09fd40a7a4c8ed15f935c2bd9e01e7869e831d
Matt Burton <mattwburton@gmail.com>
2021-12-13 15:11:35 +0000

    userMnodel.js configured

---
+++ src/user/userModel.js
@@ -0,0 +1,23 @@
+//require mongoose (already installed when connection.js set up)
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
