3ec471b28ddd78bd943e0508477d393bf702ad80
Matt Burton <mattwburton@gmail.com>
2021-12-13 14:30:23 +0000

    pre dynamic changes

--- src/server.js
+++ src/server.js
@@ -4,10 +4,13 @@ const app = express();
 
 const port = 5000;
 
-app.use("/static", express.static("public"));
+// the app.use() funtion is used to mount the specified middleware function(s) at the path specified
+app.use("/", express.static("public"));
 app.use("/aboutus", express.static("public/aboutus.html"));
+app.use("/contact", express.static("public/contact.html"));
 app.use("/giftstore", express.static("public/giftstore.html"));
 
+// the app.listen() function is used to bind and listen to the connection on the specified host and port
 app.listen(port, () => {
     console.log("listening on port 5000");
 });
